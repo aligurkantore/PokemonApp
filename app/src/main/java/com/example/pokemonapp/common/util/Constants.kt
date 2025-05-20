5b8d4fa1a491621b984c8cf24e3ee08f08be7d3c
AliGurkanTore98 <torealigurkan@gmail.com>
2025-05-20 19:04:00 +0300

    #Refactored variables to use const val.

--- app/src/main/java/com/example/pokemonapp/common/util/Constants.kt
+++ app/src/main/java/com/example/pokemonapp/common/util/Constants.kt
@@ -1,8 +1,12 @@
 package com.example.pokemonapp.common.util
 
 class Constants {
-    companion object{
+    companion object {
         const val BASE_URL = "https://pokeapi.co/api/v2/"
         const val TIME_OUT = 15L
+
+        //SERVICE
+        const val POKEMON = "pokemon"
+        const val POKEMON_DETAIL = "pokemon/{name}"
     }
 }
--- app/src/main/java/com/example/pokemonapp/data/remote/Service.kt
+++ app/src/main/java/com/example/pokemonapp/data/remote/Service.kt
@@ -1,5 +1,7 @@
 package com.example.pokemonapp.data.remote
 
+import com.example.pokemonapp.common.util.Constants.Companion.POKEMON
+import com.example.pokemonapp.common.util.Constants.Companion.POKEMON_DETAIL
 import com.example.pokemonapp.data.model.pokemondetail.PokemonDetailResponse
 import com.example.pokemonapp.data.model.pokemonlist.PokemonListResponse
 import retrofit2.Response
@@ -9,13 +11,13 @@ import retrofit2.http.Query
 
 interface Service {
 
-    @GET("pokemon")
+    @GET(POKEMON)
     suspend fun getPokemonList(
         @Query("limit") limit : Int,
         @Query("offset") offset: Int
     ) : Response<PokemonListResponse>
 
-    @GET("pokemon/{name}")
+    @GET(POKEMON_DETAIL)
     suspend fun getPokemonDetail(
         @Path("name") name: String
     ): Response<PokemonDetailResponse>
